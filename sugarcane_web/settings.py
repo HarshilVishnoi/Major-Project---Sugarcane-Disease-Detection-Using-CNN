0929974a06299ba8a3efa7316c1927ea2df9388b
Harshit Vishnoi <9harshitvishnoi2003@gmail.com>
2026-03-29 16:51:19 +0530

    fix static folder

--- sugarcane_web/settings.py
+++ sugarcane_web/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
